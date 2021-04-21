f1427a88ee6550a8461571ee3acfbfb13fbf7e7a
cortexisgod <82268009+cortexisgod@users.noreply.github.com>
2021-04-22 01:09:18 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+    guildID: '818114335183208448', // sunucu idsi
+    ownerRole:"827990518688251935", // sunucu sahiplerinin rolü
+    teyitsizRolleri:["828400349848535080", "828400350452514837"], // kayıt olmayanlara verieln rol
+    jailRolu:"827990583326146622", // cezalılara verilen rol
+    enAltYetkiliRolu:["828398171705049089","828398983424507914"], // ilk yetkili permi
+    activity:"HESK", // botun durumu
+    status:"idle",
+    invitelink:"https://discord.gg/x9YwJZYEkE", //davet linki
+    symbol:"∾ ", // tag
+    voicechannel:"832377037872300042" // botun gireceği ses kanalı
+  };
+  module.exports = ayarlar;
